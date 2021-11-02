9513cda405dd46402dffb6a95c650fdd434f4f38
A.Parsa <develop.apm@gmail.com>
2021-11-02 09:47:06 +0330

    Feature(user): add user repository

---
+++ src/user/data/pgsql/user.go
@@ -0,0 +1,37 @@
+package pgsql
+
+import (
+	"github.com/apm-dev/vending-machine/domain"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID       uint
+	Username string `gorm:"uniqueIndex;size:128;column:username"`
+	Password string `gorm:"size:128;column:password"`
+	Role     string `gorm:"size:128;column:role"`
+	Deposit  uint   `gorm:"column:deposit"`
+	gorm.Model
+}
+
+func (u *User) TableName() string {
+	return "users"
+}
+
+func (u *User) FromDomain(user *domain.User) {
+	u.ID = user.Id
+	u.Username = user.Username
+	u.Password = user.Password
+	u.Role = string(user.Role)
+	u.Deposit = user.Deposit
+}
+
+func (u *User) ToDomain() *domain.User {
+	return &domain.User{
+		Id:       u.ID,
+		Username: u.Username,
+		Password: u.Password,
+		Role:     domain.Role(u.Role),
+		Deposit:  u.Deposit,
+	}
+}
