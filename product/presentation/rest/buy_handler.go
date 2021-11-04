ab59fd28733e9bd252efb4529ee3f2f0ada5dbb0
A.Parsa <develop.apm@gmail.com>
2021-11-04 13:32:51 +0330

    Feature(product): add product crud & buy http handlers

---
+++ product/presentation/rest/buy_handler.go
@@ -0,0 +1,23 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/apm-dev/vending-machine/pkg/httputil"
+	"github.com/apm-dev/vending-machine/product/presentation/rest/requests"
+	"github.com/labstack/echo"
+)
+
+func (p *ProductHandler) Buy(c echo.Context) error {
+	req := new(requests.Buy)
+	err := httputil.BindAndValidate(c, req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, httputil.MakeResponse(
+			http.StatusBadRequest, err.Error(), nil,
+		))
+	}
+
+	bill, err := p.ps.Buy(c.Request().Context(), req.Cart)
+
+	return checkErrorThenResponse(c, err, bill)
+}
